648588b0e680d5c0ad59937c590e4ff4b66fa159
fame issrani <fame.issrani@nuance.com>
2021-06-16 16:36:50 +0530

    Added Solution for NQueens Problem no 51 from Leetcode

---
+++ 3]. Competitive Programming/LeetCode/NQueens_51.java
@@ -0,0 +1,74 @@
+
+class NQueens_51 {
+    public static List<List<String>> solveNQueens(int n) {
+        // create n x n Board
+        int[][] board = new int[n][n];
+        List<List<String>> result = new ArrayList<>();
+        solveNQueensHelper(board, 0, new ArrayList<String>(), result);
+        return result;
+    }
+
+    private static void solveNQueensHelper(int[][] board, int row, ArrayList<String> list, List<List<String>> result) {
+        if(row == board.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int col = 0; col < board[0].length; col++) {
+            if (isValid(board, row, col)) {
+
+                board[row][col] = 1;
+                String queenPosition = getQueenPosition(board[0].length, col);
+                list.add(queenPosition);
+
+                solveNQueensHelper(board, row + 1, list, result);
+
+                list.remove(list.size() - 1);
+                board[row][col] = 0;
+            }
+        }
+    }
+
+    private static boolean isValid(int[][] board, int row, int col) {
+        // check in all the boxes straight up the given row and column
+        for(int i = row - 1, j = col; i>=0; i--) {
+            if(board[i][j] == 1) {
+                return false;
+            }
+        }
+
+        // checking above left diagonal to see if there is any queen present already
+        for(int i = row - 1, j = col-1; i>=0 && j >= 0; i--, j--) {
+            if(board[i][j] == 1) {
+                return false;
+            }
+        }
+
+        // checking above right diagonal to see if there is any queen present already
+        for(int i = row - 1, j = col + 1; i>=0 && j < board.length; i--,j++) {
+            if(board[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String getQueenPosition(int stringLength, int queenPosition) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < stringLength; i++) {
+            if (queenPosition == i) {
+                sb.append("Q");
+            } else {
+                sb.append(".");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        List<List<String>> result = solveNQueens(n);
+        System.out.println(result);
+    }
+}
